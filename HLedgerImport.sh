eb319323f9456f01eaac951c2e9f34367111dcff
Zachary Rohrbach <zacharyrohrbach@Zacharys-MBP.attlocal.net>
2020-10-16 16:53:16 -0400

    various fixes

--- HLedgerImport.sh
+++ HLedgerImport.sh
@@ -10,7 +10,10 @@ function run_hledger() {
 	rm $file_for_rules
 }
 
-function deal_with_sillybank() {  #one of my bank accounts puts a lot of unnecessary gobbledygook prior to each transaction.  This function gets rid of that gobbledygook.
+#one of my bank accounts puts a lot of unnecessary 
+#gobbledygook prior to each transaction.  This function 
+#gets rid of that gobbledygook.
+function deal_with_sillybank() {  
 	file_for_rules=$1
 	temporary_file=~/temporary-sillybank-import.csv
 	cat $import_file | sed 's/MERCHANT PURCHASE TERMINAL [0-9]* //g' | sed 's/POS PURCHASE TERMINAL [0-9]* //g' | cut -c -70 > $temporary_file
@@ -18,29 +21,17 @@ function deal_with_sillybank() {  #one of my bank accounts puts a lot of unneces
 	rm $temporary_file 
 }
 
-if [ "$account" = 'citi' ]; then
+#this following if statement needs to be copied for each bank that you have
+if [ "$account" = 'bank-1' ]; then
 	run_hledger $import_file ~/.financialscripts/hledger-rules/citi.csv
 fi
 
-if [ "$account" = 'oldnatl-check' ]; then
-	run_hledger $import_file ~/.financialscripts/hledger-rules/onb.csv
-fi
 
-if [ "$account" = 'oldnatl-sav' ]; then
-	run_hledger $import_file ~/.financialscripts/hledger-rules/onb-s.csv
-fi
-
-if [ "$account" = 'lcb-e' ]; then
-	deal_with_sillybank ~/.financialscripts/hledger-rules/lcb-e.csv
-fi
-
-if [ "$account" = 'lcb' ]; then
+#use this if statement if your bank needs the gobbledygook stripped out of it.
+if [ "$account" = 'sillybank' ]; then
 	deal_with_sillybank ~/.financialscripts/hledger-rules/lcb-joint.csv
 fi
 
-if [ "$account" = 'capone' ]; then
-	run_hledger $import_file ~/.financialscripts/hledger-rules/capone.csv
-fi
 
 if [ "$import_file" = 'help' ]; then
 	echo "USAGE: $0 import_file account"
